790db05d5675219f55ca21187cc2072b02aa9379
Jakub Cabala <jcabala@jcabala.localdomain>
2022-12-09 20:40:01 +0100

    Fundtions withdrawToreceiver, withdrawToOwner, registerReceiver & fund implemented. Error about storage and memory variables in withdrawToReceiver  to resolve.

--- contracts/FundMeAdministration.sol
+++ contracts/FundMeAdministration.sol
@@ -6,6 +6,9 @@ import "./PriceConverter.sol";
 
 error FundMeAdministrator__NotOwner();
 error FundMeAdministrator__NotReceiver();
+error FundMeAdministrator__NotMinimalFee();
+error FundMeAdministrator__NotMinimalFund();
+error FundMeAdministrator__AlreadyAReceiver();
 
 contract FundMeAdministrator {
     using PriceConverter for uint256;
@@ -13,7 +16,7 @@ contract FundMeAdministrator {
     struct FundTransaction {
         address from;
         address to;
-        address amount;
+        uint amount;
     }
 
     struct Receiver {
@@ -21,16 +24,39 @@ contract FundMeAdministrator {
         string description;
     }
 
+    modifier minimalFeeMet() {
+        if (msg.value.getConversionRate(i_priceFeed) < MINIMAL_FEE)
+            revert FundMeAdministrator__NotMinimalFee();
+        _;
+    }
+
+    modifier minimalFundMet() {
+        if (msg.value.getConversionRate(i_priceFeed) < MINIMAL_FUND)
+            revert FundMeAdministrator__NotMinimalFund();
+        _;
+    }
+
     modifier onlyOwner() {
         if (msg.sender != i_owner) revert FundMeAdministrator__NotOwner();
         _;
     }
 
-    modifier onlyReceiver() {
+    modifier notAReceiver() {
         Receiver[] memory receivers = s_receivers;
-        if (receivers.length == 0) revert FundMeAdministrator__NotReceiver();
 
         bool isPresent = false;
+        for (uint i = 0; i < receivers.length; i++) {
+            address curAddress = receivers[i].receiverAddress;
+            if (curAddress == msg.sender) {
+                revert FundMeAdministrator__AlreadyAReceiver();
+            }
+        }
+        _;
+    }
+
+    modifier onlyReceiver() {
+        Receiver[] memory receivers = s_receivers;
+
         for (uint i = 0; i < receivers.length; i++) {
             address curAddress = receivers[i].receiverAddress;
             if (curAddress == msg.sender) {
@@ -43,14 +69,75 @@ contract FundMeAdministrator {
     }
 
     Receiver[] public s_receivers;
-    mapping(address => uint256) public s_addressToAmountReceived;
-    FundTransaction[] public s_fundTransactions;
+    FundTransaction[] public s_notWithdrawnTransactions;
 
     address private immutable i_owner;
     AggregatorV3Interface private immutable i_priceFeed;
+    uint public constant MINIMAL_FEE = 20;
+    uint public constant MINIMAL_FUND = 5;
 
     constructor(address priceFeedAddress) {
         i_owner = msg.sender;
         i_priceFeed = AggregatorV3Interface(priceFeedAddress);
+    }
+
+    function registerReceiver(
+        string memory description
+    ) public payable minimalFeeMet notAReceiver {
+        Receiver memory newReceiver = Receiver(msg.sender, description);
+        s_receivers.push(newReceiver);
+    }
+
+    function fund(address receiver) public payable minimalFundMet {
+        FundTransaction memory newTransaction = FundTransaction(
+            msg.sender,
+            receiver,
+            msg.value
+        );
+
+        s_notWithdrawnTransactions.push(newTransaction);
+    }
+
+    function withdrawToTheOwner() public onlyOwner {
+        FundTransaction[]
+            memory notWithdrawnTransactions = s_notWithdrawnTransactions;
+
+        uint allEth = address(this).balance;
+        uint receiversEth = 0;
+        for (uint i = 0; i < notWithdrawnTransactions.length; i++) {
+            receiversEth += notWithdrawnTransactions[i].amount;
+        }
+
+        uint ownerEth = allEth - receiversEth;
+        (bool callSuccess, ) = payable(i_owner).call{value: ownerEth}("");
+        require(callSuccess, "Call failed");
+    }
+
+    function withdrawToTheReceiver() public onlyReceiver {
+        FundTransaction[]
+            memory notWithdrawnTransactions = s_notWithdrawnTransactions;
+        FundTransaction[] memory newTransactions = new FundTransaction[](0);
+        Receiver[] memory receivers = s_receivers;
+        Receiver[] memory newReceivers = new Receiver[](0);
+
+        uint receiversFundingEth = 0;
+        for (uint i = 0; i < notWithdrawnTransactions.length; i++) {
+            if (msg.sender == notWithdrawnTransactions[i].to) {
+                receiversFundingEth += notWithdrawnTransactions[i].amount;
+            } else {
+                newTransactions.push(notWithdrawnTransactions[i]);
+            }
+        }
+
+        for (uint i = 0; i < receivers.length; i++) {
+            if (receivers[i].receiverAddress != msg.sender) {
+                newReceivers.push(receivers[i]);
+            }
+        }
+
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: receiversFundingEth
+        }("");
+        require(callSuccess, "Call failed");
     }
 }
